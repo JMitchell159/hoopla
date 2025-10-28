ad9cd1d70fc698998f5a596dd6f6e7588c31ce4e
JMichell159 <joemitchell159@gmail.com>
2025-10-28 06:12:13 -0500

    keyword search now matches any part of the query, not just the exact string

--- cli/keyword_search.py
+++ cli/keyword_search.py
@@ -1,11 +1,25 @@
 import json
+import string
 
 def kw_search(query):
     movie_file = open("data/movies.json")
     movies = json.load(movie_file)
+    punc_map = {}
+    for p in string.punctuation:
+        punc_map[p] = None
+    translator = str.maketrans(punc_map)
     results = []
+    split = query.lower().translate(translator).split(" ")
+    refined = []
+    idx = 0
+    for s in split:
+        if len(s) > 1:
+            refined.append(s)
+            idx += 1
     for m in movies["movies"]:
-        if query.lower() in m["title"].lower():
-            results.append(m)
+        for word in refined:
+            if word in m["title"].lower().translate(translator):
+                results.append(m)
+                break
     results.sort(key=lambda x: x["id"])
     return results[:5]
